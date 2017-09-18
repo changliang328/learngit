24196f65d598266a34cabb909c21acea665d279f
changliang <changliang328@163.com>
2017-09-18 16:44:32 +0800

    金点匠项目提交

---
+++ 915微掌时代_金点匠/vendor/microhand/Fingerprint/bl5192/Android.mk
@@ -0,0 +1,44 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := blestech.fingerprint.default
+LOCAL_SRC_FILES_64 := ./libs/arm64-v8a/blestech.fingerprint.default.so
+LOCAL_SRC_FILES_32 := ./libs/armeabi-v7a/blestech.fingerprint.default.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libBtlFpHal
+LOCAL_SRC_FILES_64 := ./libs/arm64-v8a/libBtlFpHal.so
+LOCAL_SRC_FILES_32 := ./libs/armeabi-v7a/libBtlFpHal.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libBtlAlgo
+LOCAL_SRC_FILES_64 := ./libs/arm64-v8a/libBtlAlgo.so
+LOCAL_SRC_FILES_32 := ./libs/armeabi-v7a/libBtlAlgo.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libxuFPAlg
+LOCAL_SRC_FILES_64 := ./libs/arm64-v8a/libxuFPAlg.so
+LOCAL_SRC_FILES_32 := ./libs/armeabi-v7a/libxuFPAlg.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
+
+
---
+++ wei掌时代金点匠/for_all_chip7.0/kernel/bl5192/bl_presure_vkeys/bl_presure_vkeys.h
@@ -0,0 +1,42 @@
+#ifndef _BL_PRESURE_VKEYS_H_
+#define _BL_PRESURE_VKEYS_H_
+//#include "tpd.h"
+#include "bl_chip.h"
+/**************************************/
+struct bl_presure_data {
+	struct i2c_client *client;
+	struct input_dev *inputdev;
+	u32 chipid;
+    u32 presure_irq_gpio;
+    u32 presure_irq_num;
+    u32 presure_reg_addr;
+    u32 leftkey_reg_addr;
+    u32 rightkey_reg_addr;
+	u32 fw_version_high;
+	u32 fw_version_low;
+    struct notifier_block fb_notify;
+	struct proc_dir_entry		*proc_entry;
+};
+/**************************************/
+int bl_getPresure(int type);
+static inline int bl_get_sensor_version(void);
+static int bl_check_update_fw(void);
+
+
+/**************************************/
+#define PRESURE_SLAVE_ADRR (0x2d)//(0x28)//(0x38)
+#define PRESURE_REG_ADDR_HIGH (0x44)
+#define PRESURE_REG_ADDR_LOW (0x45)
+#define THREHOLD_PRESURE (10)//(10)
+#define PRESURE_REG_ADDR (0xb6)
+#define LEFT_KEY_REG_ADDR	(0XB3)
+#define RIGHT_KEY_REG_ADDR	(0XB4)
+#define VERSION_HIGH_REG	(0XB6)
+#define VERSION_LOW_REG	(0XB7)
+#define KEYBIT_CENTER	(1)
+#define KEYBIT_LEFT		(1<<1)
+#define KEYBIT_RIGHT	(1<<2)
+
+#define		FRAME_CMD_LEN		0x04
+/**************************************/
+#endif	//_BL_PRESURE_VKEYS_H_
